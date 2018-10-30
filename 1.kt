f4cd53bf65c87658bade3a0f005ff627d56e9c87
Univerce <505772146@qq.com>
2018-10-30 19:01:38 +0700

    Create 1.kt

---
+++ 1.kt
@@ -0,0 +1,4 @@
+1.mipmap和drawable的区别
+android 通过 mipmap 技术提前对按缩小层级生成图片预先存储在内存中，这样就提高了图片渲染的速度和质量。
+2.jar和aar的区别
+JAR 文件就是 Java Archive File，顾名思意，它的应用是与 Java 息息相关的，是 Java 的一种文档格式。只包含了class文件与清单文件 ，不包含资源文件
