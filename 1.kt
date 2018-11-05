656d6b7770f060ba42f1793a359587d5962efdf2
Univerce <505772146@qq.com>
2018-11-05 12:32:40 +0700

    Update 1.kt

--- 1.kt
+++ 1.kt
@@ -13,3 +13,4 @@ JAR 文件就是 Java Archive File，顾名思意，它的应用是与 Java 息�
 字符串常量池(String pool, String intern pool, String保留池) 是Java堆内存中一个特殊的存储区域, 当创建一个String对象时,假如此字符串值已经存在于常量池中,则不会创建一个新的对象,而是引用已经存在的对象。
 6.进程之间的优先级。 前台进程、可见进程、后台进程、不可见进程与空进程。腾讯面的时候还特意问了我进程的保活措施。
 进程保活（不死进程）
+7.Android屏幕切换 onConfigurationChanged
