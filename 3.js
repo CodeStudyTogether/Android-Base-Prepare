9503a57b607ade32ea54dcf351944a8833310cb4
Univerce <505772146@qq.com>
2019-02-26 17:57:02 +0800

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,14 @@
+JavaScript是一种运行在浏览器中的解释型的编程语言。
+
+随着HTML5在PC和移动端越来越流行，JavaScript变得更加重要了。并且，新兴的Node.js把JavaScript引入到了服务器端，JavaScript已经变成了全能型选手。
+
+为了让JavaScript成为全球标准，几个公司联合ECMA（European Computer Manufacturers Association）组织定制了JavaScript语言的标准，被称为ECMAScript标准。
+
+所以简单说来就是，ECMAScript是一种语言标准，而JavaScript是网景公司对ECMAScript标准的一种实现。
+
+那为什么不直接把JavaScript定为标准呢？因为JavaScript是网景的注册商标。
+
+不过大多数时候，我们还是用JavaScript这个词。如果你遇到ECMAScript这个词，简单把它替换为JavaScript就行了。
+
+JavaScript代码可以直接嵌在网页的任何地方，不过通常我们都把JavaScript代码放到<head>中
+
