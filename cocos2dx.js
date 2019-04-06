11497b3a39041a1bad494aeeb73a0cac6697d330
Univerce <505772146@qq.com>
2019-04-06 09:16:19 +0800

    Update cocos2dx.js

--- cocos2dx.js
+++ cocos2dx.js
@@ -1,1 +1,5 @@
+Q: Cocos Creator 是游戏引擎吗？
+A: 它是一个完整的游戏开发解决方案，包括了 cocos2d-x 引擎的 JavaScript 实现（不需要学习一个新的引擎），以及能让你更快速开发游戏所需要的各种图形界面工具
 
+Q: Cocos Creator 能开发 Cocos2d-x C++ 或者 Lua 的游戏吗？
+A: Cocos Creator 可以通过安装 C++/Lua for Creator 插件，在编辑器里编辑 UI 和场景，导出通用的数据文件，在 Cocos2d-x 引擎中进行加载运行。
