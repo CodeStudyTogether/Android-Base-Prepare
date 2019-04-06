70614085b8dd63bfeb1d385bd1da19aa3f302e89
Univerce <505772146@qq.com>
2019-04-06 13:32:08 +0800

    Update cocos2dx.js

--- cocos2dx.js
+++ cocos2dx.js
@@ -5,3 +5,21 @@ Q: Cocos Creator 能开发 Cocos2d-x C++ 或者 Lua 的游戏吗？
 A: Cocos Creator 可以通过安装 C++/Lua for Creator 插件，在编辑器里编辑 UI 和场景，导出通用的数据文件，在 Cocos2d-x 引擎中进行加载运行。
 
 https://docs.cocos.com/creator/manual/zh/
+
+现在的Javascript遵循ECMA5的规范，TypeScript是语法上对ECMA6的实现。但现在的TypeScript需要编译（不是真正的编译，其实就是语法处理）后转换成Javascript才能执行。对于Javascript来说TypeScript就像来自未来的自己
+
+JavaScript 是基于对象和事件驱动的，无需特定的语言环境，只需在支持的浏览器上就能运行。
+
+JavaScript 是一种脚本编写语言，无需编译，只要嵌入 HTML 代码中，就能由浏览器逐行加载解释执行。
+
+TypeScript 是 Microsoft 开发和维护的一种面向对象的编程语言。它是 JavaScript 的超集，包含了 JavaScript 的所有元素，可以载入 JavaScript 代码运行，并扩展了 JavaScript 的语法。
+
+TypeScript 是 Microsoft 推出的开源语言，使用 Apache 授权协议
+TypeScript 增加了静态类型、类、模块、接口和类型注解
+TypeScript 可用于开发大型的应用
+
+TypeScript 中的数据要求带有明确的类型，JavaScript不要求。
+
+TypeScript 代码需要被编译（输出 JavaScript 代码），这是 TypeScript 代码执行时的一个额外的步骤。
+
+TypeScript 正在成为开发大型编码项目的有力工具。因为其面向对象编程语言的结构保持了代码的清洁、一致和简单的调试。因此在应对大型开发项目时，使用 TypeScript 更加合适。如果有一个相对较小的编码项目，似乎没有必要使用 TypeScript，只需使用灵活的 JavaScript 即可。
