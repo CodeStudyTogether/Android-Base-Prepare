b31d9364f374fc1b2c4bbe932c68852ee409ba9f
Univerce <505772146@qq.com>
2019-04-09 16:53:42 +0800

    Update egret.ts

--- egret.ts
+++ egret.ts
@@ -1,3 +1,25 @@
 https://wangdoc.com/javascript/basic/introduction.html
 
 https://ts.xcatliu.com/introduction/get-typescript.html
+
+表达式不需要分号结尾。一旦在表达式后面添加分号，则 JavaScript 引擎就将表达式视为语句，这样会产生一些没有任何意义的语句。
+
+var a = 1;
+
+undefined是一个特殊的值，表示“无定义”。
+
+JavaScript 是一种动态类型语言，也就是说，变量的类型没有限制，变量可以随时更改类型。
+
+if (m === 0) {
+  // ...
+} else if (m === 1) {
+  
+数值（number）：整数和小数（比如1和3.14）
+字符串（string）：文本（比如Hello World）。
+布尔值（boolean）：表示真伪的两个特殊值，即true（真）和false（假）
+undefined：表示“未定义”或不存在，即由于目前没有定义，所以此处暂时没有任何值
+null：表示空值，即此处的值为空。
+对象（object）：各种值组成的集合。
+
+https://wangdoc.com/javascript/types/null-undefined-boolean.html
+https://ts.xcatliu.com/introduction/get-typescript.html
