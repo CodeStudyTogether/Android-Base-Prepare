7fb96398b43b7fb9087b35283e03468f49b9ec6d
Univerce <505772146@qq.com>
2019-07-02 14:14:12 +0800

    Update 20190702.java

--- prepare/20190702.java
+++ prepare/20190702.java
@@ -1,1 +1,17 @@
+getdefault采用单例模式和建造者模式构建EventBus的实例,在构建实例的时候初始化了线程池
 
+post的作用就是将事件添加到PostingThreadState的事件队列中去，然后遍历这个List,调用postSingleEvent,
+既然要发送单个事件，就要知道订阅这个事件的所有方法，通过subscriptionsByEventType拿到Subscriptions的COW,接着遍历这个线程安全的类，拿到SUbscription，然后调用postToSubscrition,在这个方法里进行了线程调度，
+最终都会调用invokeSubscriber，在这里通过反射调用方法
+
+HttpDNS,IP直连
+Http 2.0多路复用
+HTTP持久连接（HTTP persistent connection，也称作HTTP keep-alive或HTTP connection reuse）是使用同一个TCP连接来发送和接收多个HTTP请求/应答，而不是为每一个新的请求/应答打开新的连接的方法。
+https://www.nihaoshijie.com.cn/index.php/archives/698/
+服务器要配置
+HTTP2的请求的TCP的connection一旦建立，后续请求以stream的方式发送。
+2）每个stream的基本组成单位是frame（二进制帧），每种frame又分为很多种类型例如HEADERS Frame（头部帧），DATA Frame（内容帧）等等。
+
+3）请求头HEADERS Frame组成了resquest，返回头HEADERS Frame和DATA Frame组成了response，request和response组成了一个stream。
+
+HTTP2采用多路复用是指，在同一个域名下，开启一个TCP的connection，每个请求以stream的方式传输，每个stream有唯一标识，connection一旦建立，后续的请求都可以复用这个connection并且可以同时发送，server端可以根据stream的唯一标识来相应对应的请求。
