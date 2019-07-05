97dd75d572af530e33347af6772baf84f1930e8e
Univerce <505772146@qq.com>
2019-07-05 11:53:46 +0800

    Update 20190705.java

--- prepare/20190705.java
+++ prepare/20190705.java
@@ -1,1 +1,5 @@
+ 加入了对刘海屏的支持
+ 
+ Android Pie引入了大量新的安全和隐私特性，以帮助您保持数据的安全性。更新中引入的最有用的特性之一是，当你的麦克风、摄像头或传感器空闲时，应用程序将不再能够访问它们。所有应用都使用HTTPS，这是对Android安全用户的肯定。AndroidPie上的所有应用程序默认都使用HTTPS。Android关心您的隐私。
 
+   你可以在Settings>System > Display > Advanced > Devicetheme中选中“Dark“项来打开该功能，来让Android设备上变暗。
